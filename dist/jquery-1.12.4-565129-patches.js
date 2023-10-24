7d696a4677ec6a9930928b64f4ebde39b2dbffba
Varis Darasirikul <v.darasirikul@architrave.de>
2023-10-25 01:22:25 +0200

    Delete console log

--- dist/jquery-1.12.4-565129-patches.js
+++ dist/jquery-1.12.4-565129-patches.js
@@ -13,8 +13,6 @@
  */
 
 (function( global, factory ) {
-	console.log('This jquery is being used yolo saynoo 998877');
-
 	if ( typeof module === "object" && typeof module.exports === "object" ) {
 		// For CommonJS and CommonJS-like environments where a proper `window`
 		// is present, execute the factory and get jQuery.
